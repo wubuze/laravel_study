c25c66125356ca4401341beec66726e791cf56ac
wubuze <1039289613@qq.com>
2018-05-07 12:49:48 +0800

    up

--- app/Http/Controllers/Api/Access.php
+++ app/Http/Controllers/Api/Access.php
@@ -14,32 +14,38 @@ class Access extends Controller
     {
 //        config(['auth.defaults.guard'=> 'api' ]);//已经在TouteServiceProvider配置
         $this->middleware('authJwt')->except(['reg', 'login']);
+
     }
 
     public function reg()
     {
 
         $res = Client::create([
-            'name' => 'wu',
-            'account' => 'wubuze',        //
+            'name' => 'wu2',
+            'account' => 'wubuze2',        //
             'pwd' => bcrypt('123456')  //加密务必用Laravel中hepler提供的bcrypt
         ]);
 
-        return $res;
+        return  $token = auth()->login($res);//登入
+
+
+//        return $res;
     }
 
     public function login(Request $req)
     {
 
         $token = JWTAuth::attempt(['account'=> $req->account,'password'=>$req->pwd]);
+
         return response(json_encode(['token'=>$token]),200);
 
     }
 
     public function getUser(Request $req)
     {
+
 //        $user =  Auth::guard()->user();
-        $user = $req->user('api');
+        $user = $req->user();
 
 
         return $user;
---
+++ app/Http/Rep/Test.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Rep;
+
+
+
+class Test
+{
+
+    public  function test()
+    {
+        return 'test';
+    }
+
+}
